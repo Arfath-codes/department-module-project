9064517b6129add456ccd5198e6172a4f05b06ee
Arfat Ahmed <arfathcodes@gmail.com>
2025-06-16 03:23:09 +0530

    save intial project

---
+++ src/main/java/com/example/departmentmodule/DepartmentmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.departmentmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/departmentmodule/DepartmentmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.departmentmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
